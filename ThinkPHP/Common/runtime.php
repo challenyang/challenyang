0de45787998292d24ed32215ca57e5fa928e5ba4
challenyang <challenyang@qq.com>
2011-07-13 10:12:25 +0800

    add

--- ThinkPHP/Common/runtime.php
+++ ThinkPHP/Common/runtime.php
@@ -130,9 +130,10 @@ function build_app_dir() {
         // 写入测试Action
         if(!is_file(LIB_PATH.'Action/IndexAction.class.php'))
             build_action();
-    }else{
-        header("Content-Type:text/html; charset=utf-8");
-        exit('<div style=\'font-weight:bold;float:left;width:345px;text-align:center;border:1px solid silver;background:#E8EFFF;padding:8px;color:red;font-size:14px;font-family:Tahoma\'>项目目录不可写，目录无法自动生成！<BR>请使用项目生成器或者手动生成项目目录~</div>');
     }
+//    else{
+//        header("Content-Type:text/html; charset=utf-8");
+//        exit('<div style=\'font-weight:bold;float:left;width:345px;text-align:center;border:1px solid silver;background:#E8EFFF;padding:8px;color:red;font-size:14px;font-family:Tahoma\'>项目目录不可写，目录无法自动生成！<BR>请使用项目生成器或者手动生成项目目录~</div>');
+//    }
 }
 ?>
