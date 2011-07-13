3ae03cbf9509d1c4511dc21f451d33ca060942a4
challenyang <challenyang@qq.com>
2011-07-13 13:46:51 +0800

    add

--- App/Lib/Action/IndexAction.class.php
+++ App/Lib/Action/IndexAction.class.php
@@ -1,32 +1,29 @@
 <?php
+
 // 本文档自动生成，仅供测试运行
-class IndexAction extends Action
-{
+class IndexAction extends Action {
+
     /**
-    +----------------------------------------------------------
-    * 默认操作
-    +----------------------------------------------------------
-    */
-    public function index()
-    {
-        $this->display(THINK_PATH.'/Tpl/Autoindex/hello.html');
+      +----------------------------------------------------------
+     * 默认操作
+      +----------------------------------------------------------
+     */
+    public function index() {
+        $this->display();
+        //$this->display(THINK_PATH.'/Tpl/Autoindex/hello.html');
     }
 
     /**
-    +----------------------------------------------------------
-    * 探针模式
-    +----------------------------------------------------------
-    */
-    public function checkEnv()
-    {
-        load('pointer',THINK_PATH.'/Tpl/Autoindex');//载入探针函数
-        $env_table = check_env();//根据当前函数获取当前环境
+      +----------------------------------------------------------
+     * 探针模式
+      +----------------------------------------------------------
+     */
+    public function checkEnv() {
+        load('pointer', THINK_PATH . '/Tpl/Autoindex'); //载入探针函数
+        $env_table = check_env(); //根据当前函数获取当前环境
         echo $env_table;
     }
 
-    public function info()
-    {
-        $this->display();
-    }
 }
+
 ?>
