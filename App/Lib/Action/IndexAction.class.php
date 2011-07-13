2d210ae48cc5484abb07178265d41748f5e1c9dc
challenyang <challenyang@qq.com>
2011-07-13 14:10:46 +0800

    add

--- App/Lib/Action/IndexAction.class.php
+++ App/Lib/Action/IndexAction.class.php
@@ -8,7 +8,7 @@ class IndexAction extends Action {
      * 默认操作
       +----------------------------------------------------------
      */
-    public function index() {
+    public function indexxp() {
         //$this->display(THINK_PATH.'/Tpl/Autoindex/hello.html');
         load('pointer', THINK_PATH . '/Tpl/Autoindex'); //载入探针函数
         $env_table = check_env(); //根据当前函数获取当前环境
