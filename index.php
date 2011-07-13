8ca0bb3e1fcb114b756b1106a4ede9d53750da7a
challenyang <challenyang@qq.com>
2011-07-13 11:39:48 +0800

    add

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 define("THINK_PATH","./ThinkPHP");
 define("APP_NAME","App");
 define("APP_PATH","./App");
-<<<<<<< HEAD
 require THINK_PATH."/ThinkPHP.php";
-=======
-require THINK_PATH."/SaeThinkPHP.php";
->>>>>>> c3600af8c7a02f051a8ac9c4828c15a022003f98
 App::run();
 ?>
