d6f9d8cc658ef7a4e629fb2f5cb5d1eb1261d54d
challenyang <challenyang@qq.com>
2011-07-13 11:59:32 +0800

    ass

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 define('THINK_PATH', './ThinkPHP');
 define('APP_NAME', 'App');
 define('APP_PATH', './App');
-defined('RUNTIME_PATH', sys_get_temp_dir());
+define('RUNTIME_PATH', sys_get_temp_dir());
 require THINK_PATH . "/ThinkPHP.php";
 App::run();
 ?>
