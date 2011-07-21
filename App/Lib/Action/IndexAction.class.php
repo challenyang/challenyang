354581d3e711ecbd223788592c2385b3365980ea
challenyang <challenyang@qq.com>
2011-07-21 16:56:29 +0800

    add

--- App/Lib/Action/IndexAction.class.php
+++ App/Lib/Action/IndexAction.class.php
@@ -22,6 +22,10 @@ class IndexAction extends Action {
         $env_table = check_env(); //根据当前函数获取当前环境
         echo $env_table;
     }
+  
+    public function phpinfo(){
+        phpinfo();
+    }
 
 }
 
